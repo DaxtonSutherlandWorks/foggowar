b3018664cfa01db2f704d897e44deb3c25d4aad4
DaxtonSutherlandWorks <daxtonas77@gmail.com>
2025-12-31 02:01:24 -0500

    Added freehand drawing to use as a base for canvas funcitons.

--- src/components/MapEditor.js
+++ src/components/MapEditor.js
@@ -11,16 +11,42 @@ class MapEditor extends React.Component {
     constructor(props) {
         super(props);
         this.canvasRef = React.createRef();
+
+        this.brushColor = '#000000';
+        this.brushSize = 2;
+        this.painting = false;
     }
 
     componentDidMount() {
        this.context = this.canvasRef.current.getContext('2d');
+
+       this.canvasRef.current.width = this.state.dimensions[1] * 72;
+       this.canvasRef.current.height = this.state.dimensions[0] * 72;
+
+       this.canvasRef.current.addEventListener('mousedown', (event) => {
+            this.painting = true;
+            this.context.beginPath();
+            this.context.moveTo(event.layerX, event.layerY);
+        })
+
+        this.canvasRef.current.addEventListener('mouseup', () => {
+            this.painting = false;
+        })
+
+        this.canvasRef.current.addEventListener('mousemove', (event) => {
+            if (this.painting) {
+                this.context.lineTo(event.layerX, event.layerY);
+                this.context.strokeStyle = this.brushColor;
+                this.context.lineWidth = this.brushSize;
+                this.context.stroke();
+            }
+        })
     }
 
     render () {
         return ( 
-            <div style={{border: "solid 5px black"}}>
-                <canvas ref={this.canvasRef} className="map-canvas" style={{width: this.state.dimensions[1] * 72, height: this.state.dimensions[0] * 72}}></canvas>
+            <div style={{border: "solid 5px black", width: this.state.dimensions[1] * 72, height: this.state.dimensions[0] * 72}}>
+                <canvas ref={this.canvasRef} className="map-canvas"></canvas>
                 <TileMap dimensions={this.state.dimensions}></TileMap>
             </div>
         );
