2953b1797aef931ee8f83bd7962e607eba525f49
DaxtonSutherlandWorks <daxtonas77@gmail.com>
2026-08-21 21:03:04 -0400

    Redid the brush box into a general toolbox with a side bar for more specialized functions. Removed stamp brush and made it its own side bar menu.

--- src/components/BrushBox.js
+++ src/components/BrushBox.js
@@ -1,4 +1,5 @@
 import "../styles/BrushBox.css"
+import BrushIcon from "../img/brushIcon.svg"
 import LineIcon from "../img/lineIcon.svg"
 import SquareIcon from "../img/squareIcon.svg"
 import CircleIcon from "../img/circleIcon.svg"
@@ -6,11 +7,13 @@ import PolygonIcon from "../img/polygonIcon.svg"
 import StampIcon from "../img/stampIcon.svg"
 import PanIcon from "../img/panIcon.svg"
 import { getAllStamps, getStamp } from "../stamps/StampDatabase"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 
 const BrushBox = ({paintTool, paintToolSetter, paintMode, paintModeSetter, deleteMode, deleteModeSetter, loadStamp, currStamp, setCurrStamp}) => {
     
+    const [activeMenu, setActiveMenu] = useState("brush");
+
     //Sets up HTML elements for stamps.
     const stamps = getAllStamps();
 
@@ -69,14 +72,14 @@ const BrushBox = ({paintTool, paintToolSetter, paintMode, paintModeSetter, delet
         paintToolSetter("stamp");
         paintModeSetter("inactive");
 
-        document.getElementById("stamp-overlay").classList.add("open");
+        setActiveMenu("stamp")
     }
 
-    const handleStampOverlayDeactivation = (event) => {
+    const handleOverlayDeactivation = (event) => {
         paintToolSetter("line");
         paintModeSetter("inactive");
-
-        document.getElementById("stamp-overlay").classList.remove("open");
+        
+        setActiveMenu("base");
     }
 
     
@@ -95,67 +98,76 @@ const BrushBox = ({paintTool, paintToolSetter, paintMode, paintModeSetter, delet
         }
     }
 
-    return ( 
-        <div className="brush-box">
-            {/* Base Menu */}
-            <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
+    return (
+        <div className="tool-container">
             
-            <div className="brush-grid">
-                <button id="line-button" className="icon-button" disabled={paintMode === "painting"} onClick={handleBrushChange} style={{backgroundColor: paintTool === "line" & paintMode !== "panning" ? "#9e9ee2" : "#e6e6fa"}}>
-                    <img id="line-icon" src={LineIcon} alt="Line Icon"></img>
-                    <span>Line</span>
+            {/*Left Sidebar*/}
+            <div className="tool-sidebar">
+                <button id="brush-button" onClick={handleOverlayDeactivation}>
+                    <img src={BrushIcon} alt="" />
+                    <span>Brushes</span>
                 </button>
-                <button id="square-button" className="icon-button" disabled={paintMode === "painting"} onClick={handleBrushChange} style={{backgroundColor: paintTool === "square" & paintMode !== "panning" ? "#9e9ee2" : "#e6e6fa"}}>
-                    <img id="square-icon" src={SquareIcon} alt="Square Icon"></img>
-                    <span>Rectangle</span>
-                </button>
-                <button id="circle-button" className="icon-button" disabled={paintMode === "painting"} onClick={handleBrushChange} style={{backgroundColor: paintTool === "circle" & paintMode !== "panning" ? "#9e9ee2" : "#e6e6fa"}}>
-                    <img id="circle-icon" src={CircleIcon} alt="Circle Icon"></img>
-                    <span>Circle</span>
-                </button>
-                <button id="polygon-button" className="icon-button" disabled={paintMode === "painting"} onClick={handleBrushChange} style={{backgroundColor: paintTool === "polygon" & paintMode !== "panning" ? "#9e9ee2" : "#e6e6fa"}}>
-                    <img id="polygon-icon" src={PolygonIcon} alt="Polygon Icon"></img>
-                    <span>Polygon</span>
-                </button>
-                <button id="stamp-button" className="icon-button" disabled={paintMode === "painting"} onClick={handleStampOverlayActivation} style={{backgroundColor: paintTool === "stamp" & paintMode !== "panning" ? "#9e9ee2" : "#e6e6fa"}}>
-                    <img id="stamp-icon" src={StampIcon} alt="Stamp Icon"></img>
-                    <span>Stamp</span>
-                </button>
-                <button id="pan-button" className="icon-button" disabled={paintMode === "painting"} onClick={handleBrushChange} style={{backgroundColor: paintMode === "panning" ? "#9e9ee2" : "#e6e6fa"}}>
-                    <img id="pan-icon" src={PanIcon} alt="Pan Icon"></img>
-                    <span>Pan</span>
+                <button id="stamp-button" onClick={handleStampOverlayActivation}>
+                    <img src={StampIcon} alt="" />
+                    <span>Stamps</span>
                 </button>
             </div>
             
-            <div style={{display: "flex", justifyContent: "center"}}>
-                <button className="mode-button" id="draw-button" disabled={paintMode === "painting"} onClick={handleDeleteChange} style={{backgroundColor: deleteMode ? "#e6e6fa" : "#9e9ee2"}}>Draw</button>
-                <button className="mode-button" id="delete-button" disabled={paintMode === "painting"} onClick={handleDeleteChange} style={{backgroundColor: deleteMode ? "#9e9ee2" : "#e6e6fa"}}>Delete</button>
-            </div>
-
-            <div className="info-box">
-                <p><span>Line:</span> Draw a line from point A to B.</p>
-                <p><span>Rectangle:</span> Clear or fill a rectangle.</p>
-                <p><span>Circle:</span> Clear or fill a cirlce.</p>
-                <p><span>Polygon:</span> Clear or fill a polygon by placing points, ending where you started.</p>
-                <p><span>Stamp:</span> Place a stamp in an open tile.</p>
-                <p><span>Pan:</span> Drag the map to change your view.</p>
-            </div>
-        
-            {/* Overlays TODO: Remove if no more overlays are needed*/}
-            {/* Stamp Overlay */}
-            <div id="stamp-overlay" class="stamp-overlay">
-                <div class="stamp-overlay-header">
-                    <h3>Stamp Library</h3>
-                    <button class="close-button" onClick={handleStampOverlayDeactivation}>
-                        X
+            {/* Main Tool Box */}
+            <div className="brush-box">
+                {/* Base Menu */}
+                <div className="brush-grid">
+                    <button id="line-button" className="icon-button" disabled={paintMode === "painting"} onClick={handleBrushChange} style={{backgroundColor: paintTool === "line" & paintMode !== "panning" ? "#9e9ee2" : "#e6e6fa"}}>
+                        <img id="line-icon" src={LineIcon} alt="Line Icon"></img>
+                        <span>Line</span>
+                    </button>
+                    <button id="square-button" className="icon-button" disabled={paintMode === "painting"} onClick={handleBrushChange} style={{backgroundColor: paintTool === "square" & paintMode !== "panning" ? "#9e9ee2" : "#e6e6fa"}}>
+                        <img id="square-icon" src={SquareIcon} alt="Square Icon"></img>
+                        <span>Rectangle</span>
+                    </button>
+                    <button id="circle-button" className="icon-button" disabled={paintMode === "painting"} onClick={handleBrushChange} style={{backgroundColor: paintTool === "circle" & paintMode !== "panning" ? "#9e9ee2" : "#e6e6fa"}}>
+                        <img id="circle-icon" src={CircleIcon} alt="Circle Icon"></img>
+                        <span>Circle</span>
+                    </button>
+                    <button id="polygon-button" className="icon-button" disabled={paintMode === "painting"} onClick={handleBrushChange} style={{backgroundColor: paintTool === "polygon" & paintMode !== "panning" ? "#9e9ee2" : "#e6e6fa"}}>
+                        <img id="polygon-icon" src={PolygonIcon} alt="Polygon Icon"></img>
+                        <span>Polygon</span>
+                    </button>
+                    <button id="pan-button" className="icon-button" disabled={paintMode === "painting"} onClick={handleBrushChange} style={{backgroundColor: paintMode === "panning" ? "#9e9ee2" : "#e6e6fa"}}>
+                        <img id="pan-icon" src={PanIcon} alt="Pan Icon"></img>
+                        <span>Pan</span>
                     </button>
                 </div>
+                    
+                {/* Draw/Delete Toggles */}
                 <div style={{display: "flex", justifyContent: "center"}}>
-                    <button className="mode-button" id="draw-button" disabled={paintMode === "painting"} onClick={handleDeleteChange} style={{backgroundColor: deleteMode ? "#e6e6fa" : "#9e9ee2"}}>Add Stamp</button>
-                    <button className="mode-button" id="delete-button" disabled={paintMode === "painting"} onClick={handleDeleteChange} style={{backgroundColor: deleteMode ? "#9e9ee2" : "#e6e6fa"}}>Delete Stamp</button>
+                    <button className="mode-button" id="draw-button" disabled={paintMode === "painting"} onClick={handleDeleteChange} style={{backgroundColor: deleteMode ? "#e6e6fa" : "#9e9ee2"}}>Draw</button>
+                    <button className="mode-button" id="delete-button" disabled={paintMode === "painting"} onClick={handleDeleteChange} style={{backgroundColor: deleteMode ? "#9e9ee2" : "#e6e6fa"}}>Delete</button>
                 </div>
-                <div class="stamp-overlay-grid">
-                    {stampGridItems}
+
+                {/* Info Box */}
+                <div className="info-box">
+                    <p><span>Line:</span> Draw a line from point A to B.</p>
+                    <p><span>Rectangle:</span> Clear or fill a rectangle.</p>
+                    <p><span>Circle:</span> Clear or fill a cirlce.</p>
+                    <p><span>Polygon:</span> Clear or fill a polygon by placing points, ending where you started.</p>
+                    <p><span>Stamp:</span> Place a stamp in an open tile.</p>
+                    <p><span>Pan:</span> Drag the map to change your view.</p>
+                </div>
+            
+                {/* Overlays TODO: Remove if no more overlays are needed*/}
+                {/* Stamp Overlay */}
+                <div id="stamp-overlay" className={`stamp-overlay ${activeMenu == "stamp" ? "open" : ""}`}>
+                    <div class="stamp-overlay-header">
+                        <h3>Stamp Library</h3>
+                    </div>
+                    <div style={{display: "flex", justifyContent: "center"}}>
+                        <button className="mode-button" id="draw-button" disabled={paintMode === "painting"} onClick={handleDeleteChange} style={{backgroundColor: deleteMode ? "#e6e6fa" : "#9e9ee2"}}>Add Stamp</button>
+                        <button className="mode-button" id="delete-button" disabled={paintMode === "painting"} onClick={handleDeleteChange} style={{backgroundColor: deleteMode ? "#9e9ee2" : "#e6e6fa"}}>Delete Stamp</button>
+                    </div>
+                    <div class="stamp-overlay-grid">
+                        {stampGridItems}
+                    </div>
                 </div>
             </div>
         </div>
